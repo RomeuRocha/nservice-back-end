eaed5fe93170e29f5c264290e2070cc3bb645500
RomeuRocha <romeu.d.rocha@gmail.com>
2020-12-05 15:49:50 -0300

    Ajuste no formato da data

--- src/main/java/com/unirios/gspi/dto/OrdemServicoDTO.java
+++ src/main/java/com/unirios/gspi/dto/OrdemServicoDTO.java
@@ -37,7 +37,8 @@ public class OrdemServicoDTO implements Serializable{
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
 	private Instant saveMoment;//momento salvo;//deverá ser feito em trigger
 	
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyy-MM-dd'T'HH:mm:ss.SSS'Z'",timezone = "GMT")
+	//"yyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyy-MM-dd'T'HH:mm",timezone = "GMT")
 	private Instant dateSchedule;//data e hora do agendamento
 
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
